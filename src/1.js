d410da3227199322692de6bde87e98d6e2483b3c
jisoooo17 <jisoooo17@gmail.com>
2024-07-23 18:29:40 +0900

    컴포넌트 분리

--- src/1.js
+++ src/1.js
@@ -22,7 +22,7 @@ function App() {
 
       {/* 키비주얼 */}
       <div className="main-bg"></div>
-      {/* <div className="main-bg" style={{backgroundImage: `${bg}`}}></div> */}      
+      {/* <div className="main-bg" style={{backgroundImage: `${bg}`}}></div> */}
 
       {/* <img src={process.env.PUBLIC_URL + 'logo192.png'} alt="" /> */}
 
@@ -30,44 +30,28 @@ function App() {
       {/* 상품 리스트 */}
       <div className="container">
         <div className="flex-row">
-          {/* <div className="col">
+          <div className="col">
             <img src="https://codingapple1.github.io/shop/shoes1.jpg" alt="" />
             <h4>{console.log(shoes)}</h4>
             <h4>{shoes[0].title}</h4>
             <p>{shoes[0].price}</p>
             <p>{shoes[0].content}</p>
-          </div> */}
-
-          {
-            shoes.map((q, index)=>{
-              return(
-                <List shoes={shoes} index={index}/>
-              )
-            })
-          }
+          </div>
           
-          {/* <div className="col">
+          <div className="col">
+            <img src="https://codingapple1.github.io/shop/shoes2.jpg" alt="" />
+            <h4>상품명</h4>
+            <p>상품 설명</p>
+          </div>
+          <div className="col">
             <img src="https://codingapple1.github.io/shop/shoes3.jpg" alt="" />
             <h4>상품명</h4>
             <p>상품 설명</p>
           </div>
-
-          <List shoes={shoes}/> */}
         </div>
       </div>
     </div>
   );
-}
-
-function List(props){
-  return(
-    <div className="col">
-      <img src={`https://codingapple1.github.io/shop/shoes${props.index + 1}.jpg`} alt="" />
-      <h4>{props.shoes[props.index].title}</h4>
-      <p>{props.shoes[props.index].price}</p>
-      <p>{props.shoes[props.index].content}</p>
-    </div>
-  )
 }
 
 export default App;
