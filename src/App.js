7a87ed1ee92dfd0dc3472c1d7e69c561c9f5ec38
jisoooo17 <jisoooo17@gmail.com>
2024-07-23 18:19:13 +0900

    데이터 출력

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import {Container, Nav, Navbar} from 'react-bootstrap';
 import bg from "./img/bg.png";
-
+import { useState } from 'react';
+// import {a, b} from "./data_multi.js";
+import data from "./data.js";
 
 function App() {
+  let [shoes, setShoes] = useState(data);
+
   return (
     <div className="App">
       <Navbar bg="light" data-bs-theme="light">
@@ -16,17 +20,25 @@ function App() {
         </Container>
       </Navbar>
 
+      {/* 키비주얼 */}
       <div className="main-bg"></div>
       {/* <div className="main-bg" style={{backgroundImage: `${bg}`}}></div> */}
 
+      {/* <img src={process.env.PUBLIC_URL + 'logo192.png'} alt="" /> */}
+
+
+      {/* 상품 리스트 */}
       <div className="container">
         <div className="flex-row">
           <div className="col">
             <img src="https://codingapple1.github.io/shop/shoes1.jpg" alt="" />
-            <img src={process.env.PUBLIC_URL + 'logo192.png'} alt="" />
-            <h4>상품명</h4>
-            <p>상품 설명</p>
+            <h4>{console.log(shoes)}</h4>
+            <h4>{shoes[0].title}</h4>
+            <p>{shoes[0].price}</p>
+            <p>{shoes[0].content}</p>
           </div>
+
+          
           <div className="col">
             <img src="https://codingapple1.github.io/shop/shoes2.jpg" alt="" />
             <h4>상품명</h4>
@@ -42,5 +54,6 @@ function App() {
     </div>
   );
 }
+
 
 export default App;
